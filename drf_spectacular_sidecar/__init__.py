6741acff838672a77d29af6f1de320b6ab87bac8
T. Franzel <tfranzel@users.noreply.github.com>
2021-11-02 11:00:07 +0100

    bugfix module path typo

--- drf_spectacular_sidecar/__init__.py
+++ drf_spectacular_sidecar/__init__.py
@@ -3,4 +3,4 @@ import django
 __version__ = '2021.11.1'
 
 if django.VERSION < (3, 2):
-    default_app_config = 'drf_spectacular.apps.SpectacularSidecarConfig'
+    default_app_config = 'drf_spectacular_sidecar.apps.SpectacularSidecarConfig'
