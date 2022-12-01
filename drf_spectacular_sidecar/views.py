d0492232ae9e2726e985b46d85ce083f7758250f
Finn-Thorben Sell <dev@finn-thorben.me>
2022-12-01 11:19:08 +0100

    add a view to handle swagger ui oauth redirects

---
+++ drf_spectacular_sidecar/views.py
@@ -0,0 +1,7 @@
+from django.template.context_processors import static
+from django.views.generic import RedirectView
+
+
+class SwaggerOauthRedirectView(RedirectView):
+    def get_redirect_url(self, *args, **kwargs):
+        return static("drf_spectacular_sidecar/swagger-ui-dist/oauth2-redirect.html") + f"?{self.request.GET.urlencode()}"
